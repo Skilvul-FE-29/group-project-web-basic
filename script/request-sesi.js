1f4624ae0aef447a54816c5fe3950c40c8852e43
putridresty <putridresty25@gmail.com>
2022-10-18 20:21:08 +0700

    update login.js, req sesi features

---
+++ script/request-sesi.js
@@ -0,0 +1,7 @@
+let peserta = document.querySelector('input[name="peserta-les"]:checked');
+
+// document.getElementById('btn-pesan').addEventListener('click', (ev) => {
+//     ev.preventDefault()
+//     console.log(peserta.value)
+
+// })
