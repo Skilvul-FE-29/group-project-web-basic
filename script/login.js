7c20a531046a2fbd0eaf80db8d24eddff659123a
Fitron Ansori <fitronansori7@gmail.com>
2022-10-23 20:40:51 +0800

    update lagi

--- script/login.js
+++ script/login.js
@@ -37,4 +37,24 @@ btnLogin.addEventListener("click", (ev) => {
   validation();
 });
 
-export default users;
+// let notUserLogin = document.getElementById("not__user-login");
+// let loginUser = document.getElementById("user-login");
+// let userName = document.getElementById("user-name");
+// let linkHidden = document.querySelector(".link-hidden");
+
+// function checkUserLogin() {
+//   if (localStorage.getItem("user_login")) {
+//     notUserLogin.style.display = "none";
+//     loginUser.style.display = "block";
+//     linkHidden.style.display = "block";
+//     userName.innerHTML = JSON.parse(
+//       localStorage.getItem("user_login")
+//     ).namaLengkap;
+//   } else {
+//     notUserLogin.style.display = "block";
+//     loginUser.style.display = "none";
+//     linkHidden.style.display = "none";
+//   }
+// }
+
+// checkUserLogin();
